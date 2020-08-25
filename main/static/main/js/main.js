fcd172457914346c8068d3a90a0af214745d515b
marnikvla <condor52@yandex.ru>
2020-08-25 11:14:17 +0300

    1 add comments and clean js code 2 little improvement base.html

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -1,3 +1,5 @@
+
+//Поворот изображения ID=rotate-image
 $(document).ready(function rotate() {
     var angle = 0;
     // Поворот изображения по часовой стрелке
@@ -14,64 +16,58 @@ $(document).ready(function rotate() {
     )
 });
 
-
+//Переворот изображения (id = flip-image) лева направо
 $(".flip-horizontally-button").on("click", function () {
-    if (!$(this).hasClass('clicked-flip-horizontally-button')) { // если класса нет
-        $(this).addClass('clicked-flip-horizontally-button'); // добавляем класс
+    if (!$(this).hasClass('clicked')) { // если класса clicked нет
+        $(this).addClass('clicked'); // добавляем класс
         $("#flip-image").addClass('img-flip-horizontally');
     } else { // если есть
-        $(this).removeClass('clicked-flip-horizontally-button');
+        $(this).removeClass('clicked');
         $("#flip-image").removeClass('img-flip-horizontally'); // убираем класс
-
-
     }
 });
 
-
+//Переворот изображения (id = flip-image) ввех ногами
 $(".flip-vertically-button").on("click", function () {
-    if (!$(this).hasClass('clicked-flip-vertically-button')) { // если класса нет
-        $(this).addClass('clicked-flip-vertically-button'); // добавляем класс
-        console.log('First click');// код для первого клика
+    if (!$(this).hasClass('clicked')) { // если класса нет
+        $(this).addClass('clicked'); // добавляем класс
         $("#flip-image").addClass('img-flip-vertically');
     } else { // если есть
-        $(this).removeClass('clicked-flip-vertically-button');
+        $(this).removeClass('clicked');
         $("#flip-image").removeClass('img-flip-vertically'); // убираем класс
-
-        console.log('Second click'); // код для второго клика
-
     }
 });
 
-// function clickCounter(){
-//     let currentCount =1
-//     return function () {
-//         return currentCount++;
-//     }
-// }
-
-
-function makeCount(){
+//вспомогательная функция посчета количества кликов,
+//возвращает количество кликов деленное по модулю на n
+function makeCount(n){
     let clickCount= $(this).data("clickCount");
     if (!clickCount)
         clickCount =1
     else
         clickCount++
     $(this).data("clickCount", clickCount)
-    return clickCount%5
+    return clickCount%n
 }
 
-$('.image-backward').click(function(){
-    clickCount=makeCount()
-    console.log((clickCount))
-    console.log($("#move-image").attr('src',function (ind,currentImage) {
+//по клику на .image-backward изменяет src #move-image
+//добавляя число (clickCount) в конец названия
+//пример:
+//Russian blue0.jpg --> Russian blue1.jpg --> Russian blue2.jpg
+$('.image-move').click(function(){
+    clickCount=makeCount(5)
+    $("#move-image").attr('src',function (index,currentImage) {
         return currentImage.slice(0, currentImage.length-5)+clickCount + ".jpg"
-    }))
+    })
 })
 
+//по клику на #move-image изменяет src #move-image
+//добавляя число (clickCount) в конец названия
+//пример:
+//Russian blue0.jpg --> Russian blue1.jpg --> Russian blue2.jpg
 $('#move-image').click(function(){
-    clickCount=makeCount()
-    console.log((clickCount))
-    console.log($("#move-image").attr('src',function (ind,currentImage) {
+    clickCount=makeCount(5)
+    $("#move-image").attr('src',function (ind,currentImage) {
         return currentImage.slice(0, currentImage.length-5)+clickCount + ".jpg"
-    }))
+    })
 })
